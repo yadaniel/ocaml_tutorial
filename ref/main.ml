a51715c6be50316dca9cb55cb639cb317bf2423d
Daniel Schuele <daniel.schuele@bruker.com>
2020-06-25 13:09:43 +0200

    inner function

--- ref/main.ml
+++ ref/main.ml
@@ -23,6 +23,13 @@ let sum'' (xs: int list) =
         List.iter (fun x -> s := !s + x) xs; 
         !s;;
 
+let useModY unit : (int*int*int*int) =
+    let sum = ModY.calc ModY.Plus 0 1 in
+    let difference = ModY.calc ModY.Minus 3 1 in
+    let product = ModY.calc ModY.Mult 3 1 in
+    let quotient = ModY.calc ModY.Div 8 2 in
+    (sum, difference, product, quotient)
+
 let main init =
     Printf.printf "%s\n" "in main";
     Printf.printf "%i\n" (foobar);
@@ -39,7 +46,16 @@ let main init =
     ();
     f1 ();
     f2 ();
-    f3 ()
+    f3 ();
+    let t = useModY () in
+    let t1,t2,t3,t4 = useModY () in
+    let (t1',t2',t3',t4') = useModY () in
+    (* Printf.printf "%i,%i,%i,%i\n" (let a,b,c,d = t in a,b,c,d); (* a,b,c,d are interpreted as tuple again *)  *)
+    Printf.printf "%i,%i,%i,%i\n" (let a,_,_,_ = t in a) (let _,b,_,_ = t in b) (let _,_,c,_ = t in c) (let _,_,_,d = t in d);
+    Printf.printf "%i,%i,%i,%i\n" t1 t2 t3 t4;
+    Printf.printf "%i,%i,%i,%i\n" t1' t2' t3' t4';
+    Printf.printf "%% %@ %!";
+    ()
 
 let () = main()
 
--- ref/modY.ml
+++ ref/modY.ml
@@ -2,3 +2,37 @@
 let data : int array = [|1;2;3;4;|];;
 let data' : int list = [1;2;3;4;];;
 
+(* local function *)
+let f x y =
+    let f' x' y' = x'+y' in f' x y
+
+
+type op = Plus | Minus | Mult | Div
+
+let calc op x1 x2 =
+    let plus x y = x + y in
+    let minus x y = x - y in
+    let mult x y = x * y in
+    let div x y = x / y in
+    match op with
+        Plus -> plus x1 x2
+        | Minus -> minus x1 x2
+        | Mult -> mult x1 x2
+        | Div -> div x1 x2
+
+
+(* let rec f n m = *)
+(*     let sum = match n with *)
+(*         | 0 -> f' m *)
+(*         | _ -> n-1 *)
+(*     let rec f' n *)
+
+(* f::Int->Int->Int *)
+(* let f n m = f' 0 n m  *)
+(*     where f' acc n m =  *)
+(*         case (n,m) of *)
+(*          | (0,0) -> acc *)
+(*          | (0,_) -> f' (acc+1) n (m-1) *)
+(*          | (_,0) -> f' (acc+1) (n-1) m *)
+(*          | _ -> f' (acc+1) (n-1) m *)
+
